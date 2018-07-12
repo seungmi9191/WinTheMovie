f8ba3da45c61073a1efce0f36e8a91e8a2a85a5c
wooseungmi <joycew_wb@naver.com>
2018-07-12 15:36:51 +0900

    update 180712

--- src/main/java/kr/co/winthemovie/controller/UserController.java
+++ src/main/java/kr/co/winthemovie/controller/UserController.java
@@ -1,19 +1,18 @@
 package kr.co.winthemovie.controller;
 
 
-import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
-import kr.co.winthemovie.service.UserService;
-import kr.co.winthemovie.vo.UserVo;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import kr.co.winthemovie.service.UserService;
+import kr.co.winthemovie.vo.UserVo;
 
 @Controller
 @RequestMapping(value="/user")
